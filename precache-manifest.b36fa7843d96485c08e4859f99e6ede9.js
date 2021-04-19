fb827daa8bf1cad01e5f63d452fc6209463ded51
Paweł Benkowski <pawlo97.pb@gmail.com>
2021-04-19 18:09:59 +0200

    Updates

---
+++ precache-manifest.b36fa7843d96485c08e4859f99e6ede9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb3b1b5d01609c9402376328a807e9b",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/pwa/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d70dc9f272598e6ecc1d",
+    "url": "/pwa/static/css/main.03908442.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/pwa/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70dc9f272598e6ecc1d",
+    "url": "/pwa/static/js/main.712f2434.chunk.js"
+  },
+  {
+    "revision": "dcadc55ee51e49fdd188",
+    "url": "/pwa/static/js/runtime-main.ea7309ff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
